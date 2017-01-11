ae4ac5e2cf39e57cd518424ca4fad76772fe0109
evoup <evoex123@gmail.com>
2017-01-11 11:26:20 +0800

    init theta

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -10,34 +10,34 @@ from warmUpExercise import warmUpExercise
 import os
 
 
-# Machine Learning Online Class - Exercise 1: Linear Regression
+## Machine Learning Online Class - Exercise 1: Linear Regression
 
-#  Instructions
-#  ------------
-#
-#  This file contains code that helps you get started on the
-#  linear exercise. You will need to complete the following functions
-#  in this exericse:
-#
-#     warmUpExercise.m
-#     plotData.m
-#     gradientDescent.m
-#     computeCost.m
-#     gradientDescentMulti.m
-#     computeCostMulti.m
-#     featureNormalize.m
-#     normalEqn.m
-#
-#  For this exercise, you will not need to change any code in this file,
-#  or any other files other than those mentioned above.
-#
-# x refers to the population size in 10,000s
-# y refers to the profit in $10,000s
-#
-#
-# % Initialization
-# ==================== Part 1: Basic Function ====================
-# Complete warmUpExercise.m
+##  Instructions
+##  ------------
+##
+##  This file contains code that helps you get started on the
+##  linear exercise. You will need to complete the following functions
+##  in this exericse:
+##
+##     warmUpExercise.m
+##     plotData.m
+##     gradientDescent.m
+##     computeCost.m
+##     gradientDescentMulti.m
+##     computeCostMulti.m
+##     featureNormalize.m
+##     normalEqn.m
+##
+##  For this exercise, you will not need to change any code in this file,
+##  or any other files other than those mentioned above.
+##
+## x refers to the population size in 10,000s
+## y refers to the profit in $10,000s
+##
+##
+## % Initialization
+## ==================== Part 1: Basic Function ====================
+## Complete warmUpExercise.m
 print('Running warmUpExercise ... \n')
 print('5x5 Identity Matrix: \n')
 
@@ -46,7 +46,7 @@ warmUpExercise()
 # raw_input('Program paused. Press enter to continue.\n')
 
 
-# ======================= Part 2: Plotting =======================
+## ======================= Part 2: Plotting =======================
 print('Plotting Data ...\n')
 arr = np.loadtxt(os.getcwd() + '/ex1data1.txt', delimiter=',', usecols=(0, 1), unpack=True)
 
@@ -68,18 +68,35 @@ matplotlib.pyplot.scatter(X, y)
 
 matplotlib.pyplot.show()
 
+
+
+#raw_input('Program paused. Press enter to continue.\n');
+##%% =================== Part 3: Gradient descent ===================
 # convert to column vec
 X.shape = (97, 1)
+
+
+##X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
 
 #insert one col before it
 #np.insert param(original_matrix, offset, insert_value, insert_column_num)
 X = np.insert(X, 0, 1, axis=1)
 
 
+print X
+
+##theta = zeros(2, 1); % initialize fitting parameters
+#theta = np.array([0,0])
+#theta.shape =(2, 1)
+theta = np.array([[0],[0]]) # theta is column vec
+
+print "theta:"
+print theta
+
+
 print "done"
 
-#raw_input('Program paused. Press enter to continue.\n');
-#%% =================== Part 3: Gradient descent ===================
+
 
 
 
