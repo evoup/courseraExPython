6408a1226cdca4f4be160dd8b62f4b4814fd860e
evoup <evoex123@gmail.com>
2017-02-25 17:33:47 +0800

    clear code

--- ex3/predict.py
+++ ex3/predict.py
@@ -54,14 +54,14 @@ from sigmoid import sigmoid
 
 def predict(Theta1, Theta2, X):
     m, _ = X.shape
-    num_labels = len(Theta2)
+    # num_labels = len(Theta2)
     p = np.zeros((m, 1))
     X = np.insert(X, 0, 1, axis=1)
     for j in range(1, m + 1):
         z2 = sigmoid(np.dot(X[j - 1, :], Theta1.T))
-        z2.shape = (len(z2), 1)
-        z2 = np.insert(z2, 0, 1, axis=0)
-        pred = sigmoid(np.dot(z2.T, Theta2.T))
+        z2.shape = (1, len(z2))
+        z2 = np.insert(z2, 0, 1, axis=1)
+        pred = sigmoid(np.dot(z2, Theta2.T))
         res = pred.argmax(axis=1) + 1  # because py max index start from 0 so, 0 will be 1,9 will be 10
         p[j - 1] = res
     return p
